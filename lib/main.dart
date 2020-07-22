96fd3847aa0c5cacb9618b8e7158fccb5fe849b4
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-07-22 16:17:43 -0300

    cap 2 atividade 6 a partir min 9:06

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: ListaTransferencias(),
+          appBar: AppBar(
+            title: Text('Transferência'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('1000'),
+          ),
+        ),
+        ItemTransferencia()
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(valor),
+        subtitle: Text(numeroConta),
+      ),
+    );
+  }
+}
