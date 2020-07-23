651e08d75449d43222e2cf4abe83f0f8fda8dab7
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-07-23 07:59:26 -0300

    cap02 completo

--- lib/main.dart
+++ lib/main.dart
@@ -22,30 +22,34 @@ class ListaTransferencias extends StatelessWidget {
     // TODO: implement build
     return Column(
       children: <Widget>[
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('1000'),
-          ),
-        ),
-        ItemTransferencia()
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 1001)),
+        ItemTransferencia(Transferencia(300.0, 2000)),
       ],
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
 
+  ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
-        title: Text(valor),
-        subtitle: Text(numeroConta),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
       ),
     );
   }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
